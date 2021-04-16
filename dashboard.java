a0267f94f74ab12310aa387e6f6a0e31a68629f9
saiteja9533 <saitejaborusu@gmail.com>
2021-04-16 18:03:18 +0530

    Adding dashboard file

---
+++ dashboard.java
@@ -0,0 +1,14 @@
+public class Marathon {
+
+	public static void main(String[] args) {
+	
+		marathon();
+
+	}
+	
+	
+	public static void marathon() {
+		System.out.println("This is the Mrathon function");
+	}
+
+}
